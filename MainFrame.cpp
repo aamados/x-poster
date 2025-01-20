3814e4ffcca505b415f4d4a8181c0f28827fb982
Andrew <aamado@knights.ucf.edu>
2025-01-19 19:05:23 -0700

    Event Propogation example

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -5,36 +5,50 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
-	wxTextCtrl* txtCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(300, 375), wxSize(200, -1));
+	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
+	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 350), wxSize(200, 50));
 
 	// Bind() comes from the wxEVTHandler, which every control inherits
-	button->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	slider->Bind(wxEVT_SLIDER, &MainFrame::OnSliderChanged, this);
-	txtCtrl->Bind(wxEVT_TEXT, &MainFrame::OnTextChanged, this);
+	// Call Bind() on MainFrame
+	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
 
-	// Remove the event
-	button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);
-	
+	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
+
+	// Button-Specific events
+	button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
+	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
+
 	// Inherited from wxFrame
 	CreateStatusBar();
 }
 
-void MainFrame::OnButtonClicked(wxCommandEvent& evt)
+// evt.Skip() is called here. If it isn't, MainFrame will not close because the actual closing
+// of the frame is from the parent class, wxFrame.
+// Simply, because the child had its event called, the parent will not do so unless Skip() is called.
+void MainFrame::OnClose(wxCloseEvent& evt)
 {
-	wxLogStatus("Button Clicked");
+	wxLogMessage("Frame Closed");
+	evt.Skip();
 }
 
-void MainFrame::OnSliderChanged(wxCommandEvent& evt)
+// Since we bind the event to MainFrame, this event is invoked when ANY button is pressed.
+// This may be useful for playing a sound when ANY button is clicked for example.
+void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt)
 {
-	wxString str = wxString::Format("Slider Value: %d", evt.GetInt());
-	wxLogStatus(str);
+	// Displays a message in a box instead of in a status bar
+	wxLogMessage("Button Clicked");
 }
 
-void MainFrame::OnTextChanged(wxCommandEvent& evt)
+// Calling evt.Skip() tells wxWidgets that Event Propagation should continue.
+// In other words, we will see the specific button event called AND OnAnyButtonClicked() called.
+void MainFrame::OnButton1Clicked(wxCommandEvent& evt)
 {
-	wxString str = wxString::Format("Text: %s", evt.GetString());
-	wxLogStatus(str);
+	wxLogStatus("Button 1 Clicked");
+	evt.Skip();
 }
 
+void MainFrame::OnButton2Clicked(wxCommandEvent& evt)
+{
+	wxLogStatus("Button 2 Clicked");
+	evt.Skip();
+}
--- MainFrame.h
+++ MainFrame.h
@@ -9,8 +9,9 @@ public:
 	MainFrame(const wxString& title);
 
 private:
-	void OnButtonClicked(wxCommandEvent& evt);
-	void OnSliderChanged(wxCommandEvent& evt);
-	void OnTextChanged(wxCommandEvent& evt);
+	void OnAnyButtonClicked(wxCommandEvent& evt);
+	void OnButton1Clicked(wxCommandEvent& evt);
+	void OnButton2Clicked(wxCommandEvent& evt);
+	void OnClose(wxCloseEvent& evt);
 };
 
