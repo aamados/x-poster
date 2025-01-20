7bae8189b47e3e8f16328e77e496bbc4dfa7caea
Andrew <aamado@knights.ucf.edu>
2025-01-19 19:30:29 -0700

    Mouse Events Example

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -4,51 +4,37 @@
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 {
 	wxPanel* panel = new wxPanel(this);
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 100));
 
-	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
-	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button", wxPoint(300, 350), wxSize(200, 50));
+	// A full list of mouse events can be found on https://docs.wxwidgets.org/3.0/classwx_mouse_event.html
+	panel->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
 
-	// Bind() comes from the wxEVTHandler, which every control inherits
-	// Call Bind() on MainFrame
-	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
-
-	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
-
-	// Button-Specific events
-	button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
-	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
+	// We have to explicity call this event on the button. If we don't do this
+	// OnMouseEvent will not be called when moving your mouse on the button.
+	// This is because wxMouseEvent is not a wxCommandEvent, thus will not have
+	// Event Propagation.
+	button->Bind(wxEVT_MOTION, &MainFrame::OnMouseEvent, this);
 
 	// Inherited from wxFrame
-	CreateStatusBar();
+	wxStatusBar* bar = CreateStatusBar();
+
+	// Eabling SetDoubleBuffered to true will prevent the flickering from happening.
+	// This flickering most likely happens because wxEVT_MOTION happens to quickly.
+	bar->SetDoubleBuffered(true);
 }
 
-// evt.Skip() is called here. If it isn't, MainFrame will not close because the actual closing
-// of the frame is from the parent class, wxFrame.
-// Simply, because the child had its event called, the parent will not do so unless Skip() is called.
-void MainFrame::OnClose(wxCloseEvent& evt)
+void MainFrame::OnMouseEvent(wxMouseEvent& evt)
 {
-	wxLogMessage("Frame Closed");
-	evt.Skip();
-}
+	// Using evt.GetPosition() will return the position of the mouse
+	// relative to the object your mouse is in.
+	// Calling wxGetMousePosition() instead will provide the mouse position
+	// relative to the entire screen (including outisde of the frame).
+	wxPoint mousePos = wxGetMousePosition();
 
-// Since we bind the event to MainFrame, this event is invoked when ANY button is pressed.
-// This may be useful for playing a sound when ANY button is clicked for example.
-void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt)
-{
-	// Displays a message in a box instead of in a status bar
-	wxLogMessage("Button Clicked");
-}
+	// Calling ScreenToClient() conver the screen coordinates
+	// to frame coordinates.
+	mousePos = this->ScreenToClient(mousePos);
 
-// Calling evt.Skip() tells wxWidgets that Event Propagation should continue.
-// In other words, we will see the specific button event called AND OnAnyButtonClicked() called.
-void MainFrame::OnButton1Clicked(wxCommandEvent& evt)
-{
-	wxLogStatus("Button 1 Clicked");
-	evt.Skip();
-}
-
-void MainFrame::OnButton2Clicked(wxCommandEvent& evt)
-{
-	wxLogStatus("Button 2 Clicked");
-	evt.Skip();
+	wxString message = wxString::Format("Moust Event Detected! (x=%d) (y=%d)", mousePos.x, mousePos.y);
+	wxLogStatus(message);
 }
--- MainFrame.h
+++ MainFrame.h
@@ -9,9 +9,6 @@ public:
 	MainFrame(const wxString& title);
 
 private:
-	void OnAnyButtonClicked(wxCommandEvent& evt);
-	void OnButton1Clicked(wxCommandEvent& evt);
-	void OnButton2Clicked(wxCommandEvent& evt);
-	void OnClose(wxCloseEvent& evt);
+	void OnMouseEvent(wxMouseEvent& evt);
 };
 
